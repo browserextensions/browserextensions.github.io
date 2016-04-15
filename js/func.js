744ece16ab150297558e48bb6f977b94bc43b3e4
HaNdTriX <handtrix@gmail.com>
2016-04-15 16:55:40 +0200

    Create Page

---
+++ js/func.js
@@ -0,0 +1,31 @@
+function installExtension(url) {
+  return new Promise(function(reject, resolve) {
+    chrome.webstore.install(url, reject, resolve);
+  });
+}
+
+function initEqualheight(selector) {
+  // Resize Cards to same height
+  var $cardContent = $('.card-content');
+  $cardContent.equalheight();
+  $(window).resize(function() {
+    $cardContent.equalheight();
+  });
+}
+
+function initExtensionCard() {
+  initEqualheight('.card-content');
+  $('.card-install').click(function(event) {
+    event.preventDefault();
+
+    var id = $(this).closest('.card-extension').data('extensionId');
+    var url = "https://chrome.google.com/webstore/detail/" + id;
+
+    installExtension(url)
+      .catch(function() {
+        window.open(url, '_blank');
+      });
+
+  });
+}
+
